c6b607a7e20eecd70b26c5c23c602db41c97dfb6
pronto <vitaly.pronto@gmail.com>
2017-05-11 10:14:35 +0300

    added tests

--- src/test/java/me/interair/lexer/mr/MrParserTest.java
+++ src/test/java/me/interair/lexer/mr/MrParserTest.java
@@ -6,8 +6,16 @@ import org.junit.Test;
 public class MrParserTest {
 
     @Test
-    public void test() {
-        String query = "var sequence = map({0,n}, i -> (-1)^i / (2 * i + 1))";
+    public void testMap() {
+        parse("var sequence = map({0,n}, i -> (-1)^i / (2 * i + 1))");
+    }
+
+    @Test
+    public void testReduce() {
+        parse("var pi = 4 * reduce(sequence, 0, x y -> x + y) ");
+    }
+
+    private void parse(String query) {
         MrLexer lexer = new MrLexer(CharStreams.fromString(query));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MrParser parser = new MrParser(tokens);
